354cd3a13364e1345cb7236f025451877199c47d
chotarddaniel <31211323+chotarddaniel@users.noreply.github.com>
2018-12-05 14:26:04 +0100

    Fonction base

---
+++ PHP/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <meta charset="UTF-8">
+        <title>Pizzeria</title>
+        <script type="text/javascript" src="JS/lesFonctions.js"></script>
+        <script type="text/javascript" src="JQuery/jquery-3.1.1.min.js"></script>
+        <link rel="stylesheet" type="text/css" href="../CSS/styles.css">
+    </head>
+    <head>
+          
+    </head>
+    <body>
+        
+    </body>
+</html>
