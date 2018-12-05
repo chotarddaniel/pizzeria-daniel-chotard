83a3ec1f93f5730bcb61f454233c08629ced062a
chotarddaniel <31211323+chotarddaniel@users.noreply.github.com>
2018-12-05 15:55:46 +0100

    ajout slider et deuxieme partie

--- PHP/index.php
+++ PHP/index.php
@@ -16,31 +16,58 @@
             $sql->execute();
             $sql2 = $bdd->prepare("select * from livreurs");
             $sql2->execute();
-           
-            echo"<table>";
-            echo"<tr>";
+            $sql3 = $bdd->prepare("select * from pizzas");
+            $sql3->execute();
+           echo"<section>";
+                echo"<table>";
+                echo"<tr>";
                     echo"<th>Numéros de la commande</th>";
                     echo"<th>choix du livreur</th>";
                     echo"<th>choix du client</th>";
-            echo"</tr>";
-            echo"<tr>";
-                echo "<th><input type='text' name='lname' disabled></th>";
-                echo "<th><select id=client onchange='AfficherLesClients()'>";
-                foreach($sql->fetchAll(PDO::FETCH_ASSOC) as $ligne)
-                {  
-                    echo "<option value='".$ligne['numCli']."'>".$ligne['nomCli']."</option>";
-                }
-                echo "</select></th>";
+                echo"</tr>";
+                echo"<tr>";
+                    echo "<th><input type='text' name='lname' disabled></th>";
+                    echo "<th><select id=client onchange='AfficherLesClients()'>";
+                    foreach($sql->fetchAll(PDO::FETCH_ASSOC) as $ligne)
+                    {  
+                        echo "<option value='".$ligne['numCli']."'>".$ligne['nomCli']."</option>";
+                    }
+                    echo "</select></th>";
 
-                echo "<th><select id=livreurs onchange='AfficherLesLivreurs()'>";
-                foreach($sql2->fetchAll(PDO::FETCH_ASSOC) as $ligne)
-                {  
-                    echo "<option value='".$ligne['numLiv']."'>".$ligne['nomLiv']."</option>";
-                }
-                echo "</select></th>";
-            echo"</tr>";
-            
-            echo"</table>"
+                    echo "<th><select id=livreurs onchange='AfficherLesLivreurs()'>";
+                    foreach($sql2->fetchAll(PDO::FETCH_ASSOC) as $ligne)
+                    {  
+                        echo "<option value='".$ligne['numLiv']."'>".$ligne['nomLiv']."</option>";
+                    }
+                    echo "</select></th>";
+                echo"</tr>";
+                
+                echo"</table>";
+            echo "</section>";
+            echo"</div>";
+            echo"<section>";
+                    echo "<p>choix des pizzas</p>";
+                    echo "<div>";
+                        echo"<table>";
+                            echo"<tr>";
+                                echo"<th>Nom Pizza</th>";
+                                echo"<th>Nombres de personnes</th>";
+                                echo"<th>Prix</th>";
+                                echo"<th>Quantité</th>";
+                            echo"</tr>";
+                            
+                            foreach($sql3->fetchAll(PDO::FETCH_ASSOC) as $ligne)
+                            {  
+                                echo"<tr>";
+                                echo"<th><p>".$ligne['nomPiz']."</p></th>";
+                                echo"<th><p>".$ligne['nbPers']."</p></th>";
+                                echo"<th><p>".$ligne['prix']."</p></th>";
+                                echo"<th><input type='range' min='1' max='100' value='0'></th>";
+                            }
+                            
+                        echo"</table>";
+                    echo"</div>";
+            echo"</section>";
         ?>
     </body>
 </html>
